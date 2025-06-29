919362dbe7b97abdd9cd40166d4f65ee27c39a73
mahmoud ghaith <mahmoudsobhy8960@gmail.com>
2025-06-29 07:25:34 +0200

    finish loginScreen , registerScreen , updatePasswordScreen and updateProfileScreen ui + finish register Screen Validation

--- lib/movies.dart
+++ lib/movies.dart
@@ -32,7 +32,7 @@ class _MoviesState extends State<Movies> {
               theme: AppTheme.darkTheme,
               routes: AppRoutes.routes,
               //initialRoute: AppRoutes.onBoarding1,
-              initialRoute: AppRoutes.loginScreen,
+              initialRoute: AppRoutes.onBoarding1,
               localizationsDelegates: AppLocalizations.localizationsDelegates,
               supportedLocales: [
                 Locale('en'),
